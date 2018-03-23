7cd510973a2c937191245c381ea820dfe8d36ae5
Garret29 <szymon.piotrowski93@gmail.com>
2018-03-23 18:44:19 +0100

    first commit

---
+++ src/main/java/pl/piotrowski/PasswordManagerApp.java
@@ -0,0 +1,16 @@
+package pl.piotrowski;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PasswordManagerApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/pl/piotrowski/controller/MainViewController.java
@@ -0,0 +1,4 @@
+package pl.piotrowski.controller;
+
+public class MainViewController {
+}
